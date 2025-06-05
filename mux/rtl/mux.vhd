f013eced1bdb5ced598e3cb0700e271b7f3f1bac
Dvir Hershkovits <dvirhersh@gmail.com>
2025-06-05 11:53:44 +0300

    Mux 4-to-1 -> 8-to-1
    Signed-off-by: Dvir Hershkovits <dvirhersh@gmail.com>

--- mux/rtl/mux.vhd
+++ mux/rtl/mux.vhd
@@ -3,8 +3,8 @@ use ieee.std_logic_1164.all;
 
 entity mux is
     port (
-        IN1, IN2, IN3, IN4 : in std_logic;
-        SELECTOR           : in std_logic_vector(1 downto 0);
+        IN1, IN2, IN3, IN4, IN5, IN6, IN7, IN8 : in std_logic;
+        SELECTOR           : in std_logic_vector(2 downto 0);
         RES                : out std_logic
     );
 end mux;
@@ -13,13 +13,17 @@ architecture rtl of mux is
 
 begin
 
-    process (IN1, IN2, IN3, IN4, SELECTOR)
+    process (IN1, IN2, IN3, IN4, IN5, IN6, IN7, IN8, SELECTOR)
     begin
         case SELECTOR is
-            when "00" => RES <= IN1;
-            when "01" => RES <= IN2;
-            when "10" => RES <= IN3;
-            when "11" => RES <= IN4;
+            when "000" => RES <= IN1;
+            when "001" => RES <= IN2;
+            when "010" => RES <= IN3;
+            when "011" => RES <= IN4;
+            when "100" => RES <= IN5;
+            when "101" => RES <= IN6;
+            when "110" => RES <= IN7;
+            when "111" => RES <= IN8;
             when others => RES <= '0';
         end case;
     end process;
