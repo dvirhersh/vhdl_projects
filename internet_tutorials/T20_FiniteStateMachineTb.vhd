6d531aa0483129866fd94cde4e132da04b7e49bc
Dvir Hershkovits <dvirhersh@gmail.com>
2025-06-22 17:58:36 +0300

    FSM
    Signed-off-by: Dvir Hershkovits <dvirhersh@gmail.com>

---
+++ internet_tutorials/T20_FiniteStateMachineTb.vhd
@@ -0,0 +1,51 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity T20_FiniteStateMachineTb is
+end entity;
+
+architecture sim of T20_FiniteStateMachineTb is
+
+    constant ClockFrequency : integer := 100;
+    constant ClockPeriod    : time    := 1000 ms / ClockFrequency;
+
+    signal Clk         : std_logic := '1';
+    signal nRst        : std_logic := '0';
+    signal NorthRed    : std_logic;
+    signal NorthYellow : std_logic;
+    signal NorthGreen  : std_logic;
+    signal WestRed     : std_logic;
+    signal WestYellow  : std_logic;
+    signal WestGreen   : std_logic;
+
+    signal Counter : integer range 0 to ClockFrequency * 60;
+
+begin
+
+  i_TrafficLigths : entity work.T20_TrafficLights(rtl)
+    generic map(ClockFrequency => ClockFrequency)
+    port map (
+        Clk         => Clk,
+        nRst        => nRst,
+        NorthRed    => NorthRed,
+        NorthYellow => NorthYellow,
+        NorthGreen  => NorthGreen,
+        WestRed     => WestRed,
+        WestYellow  => WestYellow,
+        WestGreen   => WestGreen);
+            
+    Clk <= not Clk after ClockPeriod / 2;
+
+    process
+    begin
+        wait until rising_edge(Clk);
+        wait until rising_edge(Clk);
+
+        nRst <= '1';
+
+        wait;
+    end process;
+
+end architecture;
+ 
---
+++ internet_tutorials/T20_TrafficLights.vhd
@@ -0,0 +1,117 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity T20_TrafficLights is
+    generic (ClockFrequency : integer);
+    port (
+        Clk         : in  std_logic;
+        nRst        : in  std_logic;
+        NorthRed    : out std_logic;
+        NorthYellow : out std_logic;
+        NorthGreen  : out std_logic;
+        WestRed     : out std_logic;
+        WestYellow  : out std_logic;
+        WestGreen   : out std_logic);
+end entity;
+
+architecture rtl of T20_TrafficLights is
+
+    type t_State is (NorthNext, StartNorth, North, StopNorth, 
+                        WestNext, StartWest, West, StopWest);
+    signal State : t_State;
+
+    signal Counter : integer range 0 to ClockFrequency * 60;
+
+begin
+    process (Clk)
+    begin
+        if rising_edge(Clk) then
+            if nRst = '0' then
+                State       <= NorthNext;
+                Counter     <= 0;
+                NorthRed    <= '1';
+                NorthYellow <= '0';
+                NorthGreen  <= '0';
+                WestRed     <= '1';
+                WestYellow  <= '0';
+                WestGreen   <= '0';
+            else
+                -- Default values
+                NorthRed    <= '0';
+                NorthYellow <= '0';
+                NorthGreen  <= '0';
+                WestRed     <= '0';
+                WestYellow  <= '0';
+                WestGreen   <= '0';
+
+                Counter     <= Counter + 1;
+              
+                case State is
+                when NorthNext =>
+                    -- Red in all directions
+                    NorthRed <= '1';
+                    WestRed  <= '1';
+                    if Counter = ClockFrequency * 5 - 1 then
+                        Counter <= 0;
+                        State   <= StartNorth;
+                    end if;
+                when StartNorth =>
+                    NorthRed    <= '1';
+                    NorthYellow <= '1';
+                    WestRed     <= '1';
+                    if Counter = ClockFrequency * 5 - 1 then
+                        Counter <= 0;
+                        State   <= North;
+                    end if;
+                    when North =>
+                    NorthGreen <= '1';
+                    WestRed    <= '1';
+                    -- 1 min.
+                    if Counter = ClockFrequency * 60 - 1 then
+                        Counter <= 0;
+                        State   <= StopNorth;
+                    end if;                      
+                when StopNorth =>
+                    NorthYellow <= '1';
+                    WestRed     <= '1';
+                    if Counter = ClockFrequency * 5 - 1 then
+                        Counter <= 0;
+                        State   <= WestNext;
+                    end if;                     
+                when WestNext =>
+                    NorthRed <= '1';
+                    WestRed  <= '1';
+                    if Counter = ClockFrequency * 5 - 1 then
+                        Counter <= 0;
+                        State   <= StartWest;
+                    end if;                    
+                when StartWest =>
+                    NorthRed   <= '1';
+                    WestRed    <= '1';
+                    WestYellow <= '1';
+                    if Counter = ClockFrequency * 5 - 1 then
+                        Counter <= 0;
+                        State   <= West;
+                    end if;                    
+                when West =>
+                    NorthRed  <= '1';
+                    WestGreen <= '1';
+                if Counter = ClockFrequency * 60 - 1 then
+                    Counter <= 0;
+                    State   <= StopWest;
+                end if;                   
+                when StopWest =>
+                    NorthRed  <= '1';
+                    WestYellow <= '1';
+                    if Counter = ClockFrequency * 5 - 1 then
+                        Counter <= 0;
+                        State   <= NorthNext;
+                    end if;                     
+              end case;
+            end if; 
+        end if; 
+    end process;
+
+end architecture;
+ 
